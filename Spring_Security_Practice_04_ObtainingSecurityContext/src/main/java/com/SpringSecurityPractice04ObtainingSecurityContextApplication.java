08f02a3af13cb020cab86e630f84e92a9f93fa39
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-12 18:24:35 +0300

    init Practice_04 ObtainingSecurityContext

---
+++ Spring_Security_Practice_04_ObtainingSecurityContext/src/main/java/com/SpringSecurityPractice04ObtainingSecurityContextApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice04ObtainingSecurityContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice04ObtainingSecurityContextApplication.class, args);
+	}
+
+}
