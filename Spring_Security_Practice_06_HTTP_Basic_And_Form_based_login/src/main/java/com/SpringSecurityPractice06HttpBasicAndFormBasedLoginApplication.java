bf1a1cee5ea715a987e57b685eaa2382ead3a452
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-13 17:28:56 +0300

    init Practice_06_HTTP_Basic_And_Form_based_login

---
+++ Spring_Security_Practice_06_HTTP_Basic_And_Form_based_login/src/main/java/com/SpringSecurityPractice06HttpBasicAndFormBasedLoginApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice06HttpBasicAndFormBasedLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice06HttpBasicAndFormBasedLoginApplication.class, args);
+	}
+
+}
