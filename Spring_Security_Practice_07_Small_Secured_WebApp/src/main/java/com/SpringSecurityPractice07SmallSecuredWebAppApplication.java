40e82eef3ee6aa29c92e9ff2a4b58fd421374fd7
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-16 18:51:45 +0300

    init Practice_07_Small_Secured_WebApp

---
+++ Spring_Security_Practice_07_Small_Secured_WebApp/src/main/java/com/SpringSecurityPractice07SmallSecuredWebAppApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice07SmallSecuredWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice07SmallSecuredWebAppApplication.class, args);
+	}
+
+}
