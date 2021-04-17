a9aa0bafba43a6f44430b2283389cee30feda06e
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-17 19:38:15 +0300

    init Practice_08_Authorizations

---
+++ Spring_Security_Practice_08_Authorizations/src/main/java/com/SpringSecurityPractice08AuthorizationsApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice08AuthorizationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice08AuthorizationsApplication.class, args);
+	}
+
+}
