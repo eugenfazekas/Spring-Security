2aa1a0e638e89434c7c97977a26ea844048dd3a2
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-19 17:39:14 +0300

    init Practice_10_Applying_Custorm_CSRF_Protection

---
+++ Spring_Security_Practice_10_Applying_Custorm_CSRF_Protection/src/main/java/com/SpringSecurityPractice10ApplyingCustormCsrfProtectionApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice10ApplyingCustormCsrfProtectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice10ApplyingCustormCsrfProtectionApplication.class, args);
+	}
+
+}
