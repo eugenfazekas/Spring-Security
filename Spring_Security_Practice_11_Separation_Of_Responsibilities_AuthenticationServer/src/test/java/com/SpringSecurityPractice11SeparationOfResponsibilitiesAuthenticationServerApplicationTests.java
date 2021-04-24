3e7039e517f8d057a38cbe9c6d365767815175f5
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-24 22:10:53 +0300

    init Practice_11_Separation_Of_Responsibilities: Authentication & Business Logic server

---
+++ Spring_Security_Practice_11_Separation_Of_Responsibilities_AuthenticationServer/src/test/java/com/SpringSecurityPractice11SeparationOfResponsibilitiesAuthenticationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPractice11SeparationOfResponsibilitiesAuthenticationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
