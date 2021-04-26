25dea571df0a9e3427dfa1fcf89f7483ab03f5f4
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-26 19:32:28 +0300

    init Practice_12_Simple_SignOn_OAuth2_App

---
+++ Spring_Security_Practice_12_Simple_SignOn_OAuth2_App/src/main/java/com/SpringSecurityPractice12SimpleSignOnOAuth2AppApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice12SimpleSignOnOAuth2AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice12SimpleSignOnOAuth2AppApplication.class, args);
+	}
+
+}
