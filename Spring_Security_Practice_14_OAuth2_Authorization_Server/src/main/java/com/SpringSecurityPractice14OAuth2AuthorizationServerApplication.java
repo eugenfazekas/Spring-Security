19d1001711aec257c9e038e0d74310b40b00d7cb
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-28 01:40:01 +0300

    init Practice_14 OAuth2 Authorization & Resource_Server

---
+++ Spring_Security_Practice_14_OAuth2_Authorization_Server/src/main/java/com/SpringSecurityPractice14OAuth2AuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice14OAuth2AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice14OAuth2AuthorizationServerApplication.class, args);
+	}
+
+}
