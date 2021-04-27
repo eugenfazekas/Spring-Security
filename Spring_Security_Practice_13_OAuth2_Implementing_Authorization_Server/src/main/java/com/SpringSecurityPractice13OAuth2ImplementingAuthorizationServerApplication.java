55e54b2435d508373f4b20d6980b85c94291f926
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-27 18:45:19 +0300

    Practice_13 added OAuth_Authorization_Server

---
+++ Spring_Security_Practice_13_OAuth2_Implementing_Authorization_Server/src/main/java/com/SpringSecurityPractice13OAuth2ImplementingAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice13OAuth2ImplementingAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice13OAuth2ImplementingAuthorizationServerApplication.class, args);
+	}
+
+}
