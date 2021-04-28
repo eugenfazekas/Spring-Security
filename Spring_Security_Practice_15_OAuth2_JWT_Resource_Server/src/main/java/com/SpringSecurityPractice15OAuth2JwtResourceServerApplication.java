250ecc1ec1dd01c61602417c97f0c3c45a2eb6b8
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-28 18:04:06 +0300

    Practice_15 init test OAuth2 JWT Authorization and Resource Server

---
+++ Spring_Security_Practice_15_OAuth2_JWT_Resource_Server/src/main/java/com/SpringSecurityPractice15OAuth2JwtResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice15OAuth2JwtResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice15OAuth2JwtResourceServerApplication.class, args);
+	}
+
+}
