a6fbb8d37656698a2ce0c6629386919f9e7dcc1d
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-29 22:38:17 +0300

    init Practice_17_Pre_And_Post_Filterring

---
+++ Spring_Security_Practice_17_Pre_And_Post_Filterring/src/main/java/com/SpringSecurityPractice17PreAndPostFilterringApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice17PreAndPostFilterringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice17PreAndPostFilterringApplication.class, args);
+	}
+
+}
