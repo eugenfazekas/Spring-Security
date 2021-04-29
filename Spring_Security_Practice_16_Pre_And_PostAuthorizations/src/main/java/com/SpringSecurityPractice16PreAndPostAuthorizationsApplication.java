1be96d82bc691e69d003fa379270987eaf53dde5
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-04-29 17:26:05 +0300

    init Practice_16_Pre_And_PostAuthorizations

---
+++ Spring_Security_Practice_16_Pre_And_PostAuthorizations/src/main/java/com/SpringSecurityPractice16PreAndPostAuthorizationsApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice16PreAndPostAuthorizationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice16PreAndPostAuthorizationsApplication.class, args);
+	}
+
+}
