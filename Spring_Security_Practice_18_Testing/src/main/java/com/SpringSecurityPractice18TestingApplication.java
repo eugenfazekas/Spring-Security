d08ed2ea9c4d91940d85267810174e2ef34b2fd2
Eugen Fazekas <eugen.fazekas@gmail.com>
2021-05-04 19:12:12 +0300

    Pratice_18 init some basic tests

---
+++ Spring_Security_Practice_18_Testing/src/main/java/com/SpringSecurityPractice18TestingApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPractice18TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityPractice18TestingApplication.class, args);
+	}
+
+}
